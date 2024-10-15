ee495c914f999659f6fc9cae91a218b7ba88334c
Sahueducation <sahu.education.24@gmail.com>
2024-10-15 17:30:11 +0530

    Implement jsstore in manage master page

---
+++ data/data-categories.js
@@ -0,0 +1,18 @@
+let categories = [
+  {
+    id: 1,
+    name: "General Public",
+  },
+  {
+    id: 2,
+    name: "Gov. Official",
+  },
+  {
+    id: 3,
+    name: "Private Official",
+  },
+  {
+    id: 4,
+    name: "Defense Personal",
+  },
+];
---
+++ data/data-departments.js
@@ -0,0 +1,18 @@
+let departments = [
+  {
+    id: 1,
+    name: "IT",
+  },
+  {
+    id: 2,
+    name: "Accounts",
+  },
+  {
+    id: 3,
+    name: "Civil",
+  },
+  {
+    id: 4,
+    name: "Sales",
+  },
+];
